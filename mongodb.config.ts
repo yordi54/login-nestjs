322b8eb6c2e9f6acdc62de502327f279fbb984d5
Yordi Condori Escalera <70175341+yordi54@users.noreply.github.com>
2023-04-18 18:58:50 -0400

    Delete mongodb.config.ts

--- mongodb.config.ts
+++
@@ -1,7 +0,0 @@
-export const mongodbConfig = {
-    uri: 'mongodb+srv://yordice77:R0EqnPj9BMYhrHjy@mongodb-topicos.9xnj6yo.mongodb.net/db_topicos?retryWrites=true&w=majority', // La URL de conexión a tu base de datos de MongoDB
-    useNewUrlParser: true, 
-    useUnifiedTopology: true, 
-    useCreateIndex: true, 
-  };
-  
